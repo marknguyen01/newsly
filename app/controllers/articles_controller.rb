2944f79605616a2ef151b4dcea5f094f0ee5ceb5
Mark Nguyen <nnguye59@uncc.edu>
2018-10-24 19:42:56 +0000

    Fixed route and controller for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,7 @@
 class ArticlesController < ApplicationController
+    def show
+        @comments = Comment.find(params[:id])
+        @likes = Like.find(params[:id])
+        @data = getJson
+    end
 end
