accda2f01603238c17523cdaba8a1304b69ed71a
marknguyen01 <nnguye59@uncc.edu>
2018-11-06 20:32:39 +0000

    Added date converion for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    helper_method :convertDate
     def show
         @article = Article.find_by(slug: params[:slug])
     end
@@ -6,6 +7,27 @@ class ArticlesController < ApplicationController
     def index
         run_schedule
         @articles = Article.order("created_at DESC").page(params[:page]).per(20)
+    end
+    
+    def convertDate(date)
+        dateNow = DateTime.now
+        dateBefore = DateTime.parse(date)
+        
+        secDiff = dateNow.to_time - dateBefore.to_time
+        minDiff = (secDiff / 60).floor
+        hourDiff = (minDiff / 60).floor
+        dayDiff = (hourDiff / 24).floor
+        # less than a minute
+        if secDiff >= 0 and secDiff < 60
+            return secDiff.to_s;
+        # less than an hour
+        elsif minDiff >= 0 and minDiff < 60
+            return minDiff.to_s + " minutes ago"
+        elsif hourDiff >= 0 and hourDiff < 24
+            return hourDiff.to_s + " hours ago"
+        elsif dayDiff >= 0 and dayDiff < 30
+            return dayDiff.to_s + " days ago"
+        end
     end
     def createComment
         
