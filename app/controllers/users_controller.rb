13e06b4c34a4ef09666457f817dad5412a0529e4
marknguyen01 <nnguye59@uncc.edu>
2018-11-12 23:20:30 +0000

    Fixed user authentication for editting profile and deleting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,17 +20,34 @@ class UsersController < ApplicationController
   end
   
   def edit
-    if current_user.id.to_s == params[:id] && !current_user.nil?
-      @user = User.find(params[:id])
-      render :edit
+    authenticate_user(current_user)
+    @user = User.find(params[:id])
+    render :edit
+  end
+  
+  def update
+    authenticate_user(current_user)
+    if User.update(users_params)
+        flash[:success] = "Your account has been updated"
+        redirect_back(fallback_location: root_path)
     else
-      flash[:danger] = "You don't have permission to view this"
+      flash[:danger] = "No permission"
       redirect_to root_path
     end
   end
+  def destroy
+    authenticate_user(current_user)
+    User.find(params[:id]).destroy
+  end
   
-  private
-  def users_params
+  private def users_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+  
+  private def authenticate_user(current_user)
+    if !current_user.id.to_s == params[:id] || current_user.nil?
+      flash[:danger] = "No permission"
+      redirect_to root_path
+    end
   end
 end
